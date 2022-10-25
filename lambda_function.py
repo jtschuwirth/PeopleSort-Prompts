2dde9703f91e443eae58425630866edac82a53bd
jtschuwirth <jtschuwirth@uc.cl>
2022-10-25 00:18:40 -0300

    new endpoint with seed

--- lambda_function.py
+++ lambda_function.py
@@ -73,5 +73,29 @@ def addOpinion(
     response.status_code=status.HTTP_200_OK
     return "Success"
 
+@app.get(f"/{game}/phrases/room")
+def getPhrasesByRoom(
+    response: Response,
+    index: int=1,
+    level: int=1,
+    room_id: str="AAAA"
+    ):
+    try:
+        table = initTable()
+        if level not in [1,2,3]:
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="level can only be 1, 2 or 3")
+        all_phrases = getPhrasesDDB(table, level)
+        if len(all_phrases)==0:
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="error retriving phrases")
+        random.seed(room_id)
+        random.shuffle(all_phrases)
+        phrases = [all_phrases[index%len(all_phrases)]]
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    response.status_code=status.HTTP_200_OK
+    return phrases
+
 #uvicorn lambda_function:app --reload --port 8081
 lambda_handler = Mangum(app, lifespan="off")
